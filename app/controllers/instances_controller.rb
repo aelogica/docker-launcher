421739fbda5811cddff6c5823326879d4c9a6c69
Nestor Pestelos and Terence Ponce <dev+nestor+terence@aelogica.com>
2014-05-28 14:11:51 +0800

    start a container

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,9 +1,20 @@
+require 'docker'
+
 class InstancesController < ApplicationController
   def create
-    container = Docker::Container.run({
-      "Image" => "quay.io/ae_nestor/hello:latest",
-      "PublishAllPorts" => true
+    container = Docker::Container.create({
+      "Image" => "quay.io/ae_nestor/hello:latest"
     })
-    binding.pry
+    container.start({"PublishAllPorts" => true})
+    get_ports_from(container)
   end
+
+  private
+
+    def get_ports_from(container)
+      ports = container.json["NetworkSettings"]["Ports"]
+      ssh = ports["22/tcp"].first["HostPort"]
+      http = ports["3000/tcp"].first["HostPort"]
+      puts "container: #{container.id} (ssh: #{ssh}, http: #{http})"
+    end
 end
