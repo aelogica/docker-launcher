0e3da34472a0257c5633bdfdad36d885a0b7cfea
Nestor Pestelos and Terence Ponce <dev+nestor+terence@aelogica.com>
2014-05-28 13:05:03 +0800

    run one container

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,4 +1,9 @@
 class InstancesController < ApplicationController
   def create
+    container = Docker::Container.run({
+      "Image" => "quay.io/ae_nestor/hello:latest",
+      "PublishAllPorts" => true
+    })
+    binding.pry
   end
 end
