85a140a0c0bb20ec367caba5216d95b0f5a0263d
Terence Ponce <terenceponce@gmail.com>
2014-05-29 12:42:29 +0800

    Add project launch form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  resources :instances, only: [:create]
   resources :projects do
     resources :instances
   end
